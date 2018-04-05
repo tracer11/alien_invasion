824a2fdf88d6d195fea523a62e0c842c52ce3b3b
tracer11 <karolgurba22@gmail.com>
2018-04-05 14:28:50 -0500

    added main screen and settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,1 +1,24 @@
-alien_invasion.py
+import sys
+import pygame
+from settings import Settings
+
+def run_game():
+  #initialize game and create screen object
+  pygame.init()
+  ai_settings = Settings()
+  screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+  pygame.display.set_caption("Alien Invasion")
+
+  #start the main loop for the game
+  while True:
+    #watch for keyboard and mouse events
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        sys.exit()
+
+    screen.fill(ai_settings.bg_color)
+
+    #updates the screen
+    pygame.display.flip()
+
+run_game()
