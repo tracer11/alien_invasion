b5aed50b8c6e9a3d4bf4d7b19c83914ff675c14b
tracer11 <karolgurba22@gmail.com>
2018-04-06 13:51:23 -0500

    refactored some more code

--- game_functions.py
+++ game_functions.py
@@ -3,20 +3,26 @@ import pygame
 
 def check_events(ship):
   for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        sys.exit()
+    if event.type == pygame.QUIT:
+      sys.exit()
+    elif event.type == pygame.KEYDOWN:
+      check_keydown_events(event, ship)
+    elif event.type == pygame.KEYUP:
+        check_keyup_events(event, ship)
+        
 
-      elif event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_RIGHT:
-          ship.moving_right = True
-        elif event.key == pygame.K_LEFT:
-          ship.moving_left = True
+def check_keydown_events(event, ship):
+  if event.key == pygame.K_RIGHT:
+    ship.moving_right = True
+  elif event.key == pygame.K_LEFT:
+    ship.moving_left = True
 
-      elif event.type == pygame.KEYUP:
-        if event.key == pygame.K_RIGHT:
-          ship.moving_right = False
-        elif event.key == pygame.K_LEFT:
-          ship.moving_left = False
+def check_keyup_events(event, ship):
+  if event.key == pygame.K_RIGHT:
+    ship.moving_right = False
+  elif event.key == pygame.K_LEFT:
+    ship.moving_left = False
+
 
 
 def update_screen(ai_settings, screen, ship):
