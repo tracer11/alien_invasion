ca4f8c8435359eeb9550368cf8ddd247e5ba1604
tracer11 <karolgurba22@gmail.com>
2018-04-09 21:49:27 -0500

    ship can now fire bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -11,13 +12,15 @@ def run_game():
   pygame.display.set_caption("Alien Invasion")
 
   ship = Ship(ai_settings, screen)
+  bullets = Group()
 
   #start the main loop for the game
   while True:
     #checks for mouse and keyboard events
-    gf.check_events(ship)
+    gf.check_events(ai_settings, screen, ship, bullets)
     ship.update()
+    bullets.update()
     #updates the screen
-    gf.update_screen(ai_settings, screen, ship)
+    gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
