04b508a90189f44768f2a88e9eab5c7bd4fa509a
tracer11 <karolgurba22@gmail.com>
2018-04-12 21:15:25 -0500

    fleet of aliens now changes direction when at edge of screen

--- game_functions.py
+++ game_functions.py
@@ -68,8 +68,20 @@ def update_bullets(bullets):
     if bullet.rect.bottom <= 0:
       bullets.remove(bullet)
 
-def update_aliens(aliens):
+def update_aliens(ai_settings, aliens):
+  check_fleet_edges(ai_settings, aliens)
   aliens.update()
+
+def check_fleet_edges(ai_settings, aliens):
+  for alien in aliens.sprites():
+    if alien.check_edges():
+      change_fleet_direction(ai_settings, aliens)
+      break
+
+def change_fleet_direction(ai_settings, aliens):
+  for alien in aliens.sprites():
+    alien.rect.y += ai_settings.fleet_drop_speed
+  ai_settings.fleet_direction *= -1
 
 
 def update_screen(ai_settings, screen, ship, aliens, bullets):
--- settings.py
+++ settings.py
@@ -1,17 +1,20 @@
 class Settings():
 
   def __init__(self):
+    #screen settings
     self.screen_width = 1200
     self.screen_height = 800
     self.bg_color = (230, 230, 230)
-
+    #ship settings
     self.ship_speed_factor = 5
-
+    #bullet settings
     self.bullet_speed_factor = 5
     self.bullet_width = 3
     self.bullet_height = 15
     self.bullet_color = 60, 60, 60
     self.bullets_allowed = 10
-
+    #alien settings
     self.alien_speed_factor = 1
+    self.fleet_drop_speed = 10
+    self.fleet_direction = 1
 
